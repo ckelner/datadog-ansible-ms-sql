6c332d33b792874b91ad8ee2f9bb913aef99b804
ckelner <chris.kelner@gmail.com>
2018-02-08 14:24:46 -0500

    strip & lower & use 'in' instead

--- failover_check.py
+++ failover_check.py
@@ -3,9 +3,10 @@ from checks import AgentCheck
 
 class FailoverCheck(AgentCheck):
     def check(self, instance):
-        clustergrp = os.popen('powershell.exe Get-ClusterGroup -Name SQLAG1').read()
-        hostname = os.popen('powershell.exe hostname').read()
-        if clustergrp.find(hostname) != -1:
+        clustergrp = os.popen('powershell.exe Get-ClusterGroup -Name SQLAG1').read().strip().lower()
+        hostname = os.popen('powershell.exe hostname').read().strip().lower()
+
+        if hostname in clustergrp:
             # Found matching hostname - server is primary
             self.gauge('sql.server.primary', 1)
         else:
