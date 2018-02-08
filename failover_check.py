4384f11b658e98afb3e9cf234567224495b89d0a
ckelner <chris.kelner@gmail.com>
2018-02-08 03:32:02 -0500

    add custom check for failovercluster

---
+++ failover_check.py
@@ -0,0 +1,13 @@
+import os
+from checks import AgentCheck
+
+class FailoverCheck(AgentCheck):
+    def check(self, instance):
+        clustergrp = os.popen('powershell.exe Get-ClusterGroup -Name SQLAG1').read()
+        hostname = os.popen('powershell.exe hostname').read()
+        if clustergrp.find(hostname) != -1:
+            # Found matching hostname - server is primary
+            self.gauge('sql.server.primary', 1)
+        else:
+            # Didn't find matching hostname - server is not primary
+            self.gauge('sql.server.primary', 0)
